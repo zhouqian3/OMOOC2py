2499fc5fef8273167d004ea27a3d43acddb18aaf
Zhou Qian <joey.zhq@gmail.com>
2015-10-18 11:47:58 +0800

    added main.py from 42 lines but got some erros, will further update

---
+++ _src/om2py0w/0wex0/main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Quick Python Script Explanation for Programmers
+import os
+
+def main ():
+	print 'Hello World!'
+	print "This is Joey\'s Greeting!"
+
+
+	foo (5, 10)
+	print '=' * 10
+	print 'this is to execute' +os.getcdw()
+
+	counter = 0
+	counter += 1
+
+	food = ['苹果','杏子','梨','李子']
+	for i in food:
+		'I would love to get some:'+i
+
+	print 'Count to 10'
+
+	for i in range (10):
+		print i
+
+def foo (param1, secondParam):
+	res = param1+secondParam
+	print '%s plus %s equals to %s'%(param1,secondParam,res)
+	if res < 50:
+		print 'This one'
+	elif (res >= 50) and (Param1==42) or (secondParam==24):
+		print 'That one'
+	else:
+		print 'Hmmmmmm'
+	return res # this is a comment
+	''' this is multiple
+line comment '''
+
+if __name__=='__main__':
+	main()
+
